20fb64ce524f98c076039b9c643c09fb383447ac
Jack Lach <jack45k@gmail.com>
2023-01-18 19:23:41 -0500

    commit functions lab - first 6 tests passing

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+
+const feetPerBlock = 264;
+
+function distanceFromHqInBlocks(blockNumber){
+    return Math.abs(blockNumber - 42);
+}
+
+function distanceFromHqInFeet(blockNumber){
+    
+    let feetCalculation;
+    feetCalculation = (distanceFromHqInBlocks(blockNumber) * feetPerBlock);
+    return feetCalculation;
+
+}
